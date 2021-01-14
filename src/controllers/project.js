a4b63cab80e740da61476f831e7790b7a544d4b8
Cryox <51400737+StCryox@users.noreply.github.com>
2021-01-14 20:27:55 +0100

    Update project.js

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -2,34 +2,34 @@ const Project = require('../models/project.js');
 
 exports.createProject = (req, res, next) => {
     delete req.body._id;
-    const thing = new Thing({
+    const project = new Project({
       ...req.body
   });
-  thing.save()
+  project.save()
   .then(() => res.status(201).json({ message: 'Objets enregistré !'}))
   .catch(error => res.status(400).json({ error }));
 };
 
 exports.modifyProject = (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
+    Project.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
         .then(() => res.status(200).json({ message: "Objet modifié !"}))
         .catch(error => res.status(404).json({ error }));
 };
 
 exports.deleteProject = (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
+    Project.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
         .catch(error => res.status(404).json({ error }));
 };
 
 exports.getOneProject = (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
+    Project.findOne({ _id: req.params.id })
         .then(thing => res.status(200).json(thing))
         .catch(error => res.status(404).json({ error }));
 };
 
 exports.getAllProjects = (req, res, next) => {
-    Thing.find()
+    Project.find()
         .then(things => res.status(200).json(things))
         .catch(error => res.status(400).json({ error}));
 };
